a42d5040429d3b4c0db8fecbc59664318b1f5adf
Fabio Grassiotto <fabiogr@gmail.com>
2017-09-08 13:45:57 -0300

    Updated code to create temporal and per-publication bar charts. Results are now given to headlines ordered per date.

--- T2/problema1.py
+++ T2/problema1.py
@@ -6,6 +6,37 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk import NaiveBayesClassifier
 from nltk.stem import RSLPStemmer
+import matplotlib.pyplot as pyplot
+import numpy as np
+import datetime
+
+# Função utilitária para conversão de meses em português.
+# evita precisar a configuração do locale em outras máquinas.
+def monthToNumber(monthName):
+    if (monthName == 'janeiro'): 
+        return 1
+    elif (monthName == 'fevereiro'):
+        return 2
+    elif (monthName == 'março'):
+        return 3
+    elif (monthName == 'abril'):
+        return 4
+    elif (monthName == 'maio'):
+        return 5
+    elif (monthName == 'junho'):
+        return 6
+    elif (monthName == 'julho'):
+        return 7
+    elif (monthName == 'agosto'):
+        return 8
+    elif (monthName == 'setembro'):
+        return 9
+    elif (monthName == 'outubro'):
+        return 10
+    elif (monthName == 'novembro'):
+        return 11
+    else:
+        return 12
 
 # Retorna no formato de dicionário utilizado pelo classificador.
 def word_feats(word):
@@ -15,9 +46,12 @@ def word_feats(word):
 with open('manchetesBrasildatabase.csv', encoding='utf8') as csvFile:
     readCsv = csv.reader(csvFile, delimiter=',',  quotechar="'")
     org_headlines = []
+    dates = []
+    sources = []
     headlines = []
     for row in readCsv:
-        # O único campo relevante para o algoritmo é a manchete em si.
+        dates.append(datetime.datetime.strptime((row[0]+'/'+str(monthToNumber(row[1]))+'/'+row[2]), "%d/%m/%Y"))
+        sources.append(row[3])
         headline = row[4]
         org_headlines.append(headline)
         # Remover nesta etapa de pre-processamento as 'stopwords' para restringir a análise, e fazer todas as palavras minúsculas.
@@ -28,6 +62,12 @@ with open('manchetesBrasildatabase.csv', encoding='utf8') as csvFile:
         content = [st.stem(word.lower()) for word in tokenizer.tokenize(headline) if word.lower() not in stop_words]
         headlines.append(content)
 csvFile.close()
+
+# Ordenação das manchetes e fontes por data, necessário para análise temporal.
+org_headlines = [org for _, org in sorted(zip(dates, org_headlines))]
+headlines = [head for _, head in sorted(zip(dates, headlines))]
+sources = [source for _, source in sorted(zip(dates, sources))]
+dates_sorted = sorted(dates)
 
 # Leitura do corpus OpLexicon 3.0 
 #### Referência:
@@ -49,6 +89,7 @@ classifier = NaiveBayesClassifier.train(training_set)
 
 # Classificação das headlines do jornal e apresentação dos resultados.
 with open('resultados.txt', 'w', encoding='utf8') as wfile:
+    valences = []
     wfile.write('{0} {1}\n'.format('Manchete'.center(73), 'Valência (0-100%)'))
     wfile.write('{0}\n'.format("-"*91))
     for headline,org_headline in zip(headlines, org_headlines):
@@ -58,5 +99,86 @@ with open('resultados.txt', 'w', encoding='utf8') as wfile:
         # Encontra a média para as palavras classificadas e 
         # converte para escala 0-100 a valência encontrada
         valence = 100*((valSum/len(headline))+1)/2
+        valences.append(valence)
         wfile.write('{0:85} {1:2}%\n'.format(org_headline, int(valence)))
 wfile.close()
+
+# Grafíco com a dispersão das notícias ao longo do semestre
+# Primeiro obtemos a média das valências para um determinado mês.
+l = list(zip(dates_sorted, valences))
+dez16Val = []
+jan17Val = []
+feb17Val = []
+mar17Val = []
+apr17Val = []
+may17Val = []
+jun17Val = []
+jul17Val = []
+ago17Val = []
+for tup in l:
+    if (tup[0].date().month == 12 and tup[0].date().year == 2016):
+        dez16Val.append(tup[1])
+    elif (tup[0].date().month == 1 and tup[0].date().year == 2017) :
+        jan17Val.append(tup[1])
+    elif (tup[0].date().month == 2 and tup[0].date().year == 2017) :
+        feb17Val.append(tup[1])
+    elif (tup[0].date().month == 3 and tup[0].date().year == 2017) :
+        mar17Val.append(tup[1])
+    elif (tup[0].date().month == 4 and tup[0].date().year == 2017) :
+        apr17Val.append(tup[1])
+    elif (tup[0].date().month == 5 and tup[0].date().year == 2017) :
+        may17Val.append(tup[1])
+    elif (tup[0].date().month == 6 and tup[0].date().year == 2017) :
+        jun17Val.append(tup[1])  
+    elif (tup[0].date().month == 7 and tup[0].date().year == 2017) :
+        jul17Val.append(tup[1])
+    elif (tup[0].date().month == 8 and tup[0].date().year == 2017) :
+        ago17Val.append(tup[1]) 
+
+monthValAverages = []
+monthValAverages.append(np.mean(dez16Val))
+monthValAverages.append(np.mean(jan17Val))
+monthValAverages.append(np.mean(feb17Val))
+monthValAverages.append(np.mean(mar17Val))
+monthValAverages.append(np.mean(apr17Val))
+monthValAverages.append(np.mean(may17Val))
+monthValAverages.append(np.mean(jun17Val))
+monthValAverages.append(np.mean(jul17Val))
+monthValAverages.append(np.mean(ago17Val))
+
+# Criação do gráfico de barras
+N = len(monthValAverages)
+x = range(N)
+width = 0.5
+fig = pyplot.figure(figsize=(8,4))
+fig.suptitle('Valências por mês', fontsize=14)
+pyplot.xlabel('Mês')
+pyplot.ylabel('Valência (%)')
+monthLabels = ['Dez/16', 'Jan/17', 'Fev/17', 'Mar/17', 'Apr/17', 'Maio/17', 'Jun/17', 'Jul/17', 'Ago/17']
+pyplot.xticks(x, monthLabels)
+pyplot.bar(x, monthValAverages, width, color="blue")
+pyplot.savefig('valências_por_mês.jpg')
+
+# Gráfico com dispersão de acordo com a fonte da manchete.
+sourceValAverages = []
+sourceSet = set(sources)
+srcValList = list(zip(sources, valences))
+for source in sourceSet:
+    valList = []
+    for tup in srcValList:
+        if (tup[0] == source) :
+            valList.append(tup[1])
+    sourceValAverages.append(np.mean(valList))
+
+# Criação do gráfico de barras
+N = len(sourceValAverages)
+x = range(N)
+width = 0.5
+fig = pyplot.figure(figsize=(8,4))
+fig.suptitle('Valências por publicação', fontsize=14)
+pyplot.xlabel('Publicação')
+pyplot.ylabel('Valência (%)')
+sourceLabels = list(sourceSet)
+pyplot.xticks(x, sourceLabels)
+pyplot.bar(x, sourceValAverages, width, color="red")
+pyplot.savefig('valências_por_publicação.jpg')
--- T2/readme.md
+++ T2/readme.md
@@ -1,16 +1,18 @@
 T2 - Detecção de Emoções em Textos
 
-Instruções:
+Instruções para execução do código:
+
     1. Colocar no mesmo diretório os arquivos:
         
-    lexico_v3.0.txt
-    manchetesBrasildatabase.csv
-    problema1.py
+        lexico_v3.0.txt
+        manchetesBrasildatabase.csv
+        problema1.py
 
     2. Utilizar Python 3.
-    3. Instalar a biblioteca NLTK utilizando pip: 
+    3. Instalar as bibliotecas NLTK e matplotlib utilizando pip: 
 
         sudo pip3 install nltk
+        sudo pip3 install matplotlib
 
     4. Fazer o download dos dados da NLTK:
 
@@ -22,5 +24,9 @@ Instruções:
     5. Executar no terminal:
 
         python3 problema1.py
-        
-    6. Coletar os resultados da classificação das manchetes no arquivo "resultados.txt".
+
+    6. Coletar os resultados da classificação das manchetes nos arquivos:
+    
+         resultados.txt - Classificação de valência (0-100%) das manchetes, ordenadas por data.
+         valências_por_mes.jpg - Médias das valências das manchetes por mês.
+         valências_por_publicação.jpg - Médias das valências das manchetes por publicação.
