1ae86b80c9a91fa85227512499a74253c58f644c
Fabio Grassiotto <fabiogr@gmail.com>
2017-10-31 22:45:21 -0200

    Added dataset split using numpy

--- T3/Atividade 5/atividade5.py
+++ T3/Atividade 5/atividade5.py
@@ -3,6 +3,7 @@
 
 # Biblioteca pandas para carregar arquivo csv em dataframes.
 import pandas as pd
+import numpy as np
 
 def read_csv():
     "Leitura das colunas: nome do arquivo, rótulo, 56 colunas de coordenadas."
@@ -10,7 +11,6 @@ def read_csv():
     remove_cols = set(range(2,9))
     cols = [col for col in all_cols if col not in remove_cols]
     df = pd.read_csv('Faces_Disciplina\imagedb_CH_disciplina.csv', header=None, usecols=cols)
-    df.set_index(['arquivo', 'rótulo', 'p1', 'p2', 'p3'])
     return df
 
 df = read_csv()
@@ -18,3 +18,7 @@ df = read_csv()
 pd.set_option('display.max_rows', 3)
 pd.set_option('display.max_columns', 6)
 
+# Divisão do dataset em treinamento e validação
+msk = np.random.rand(len(df)) < 0.8
+training = df[msk]
+testing = df[~msk]
