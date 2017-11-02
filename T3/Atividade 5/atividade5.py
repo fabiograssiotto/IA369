4535ed858f5f8b16c5354504d04f8d69ae94deea
Fabio Grassiotto <fabiogr@gmail.com>
2017-11-01 22:53:30 -0200

    Updated code and notebook with classification

--- T3/Atividade 5/atividade5.py
+++ T3/Atividade 5/atividade5.py
@@ -4,7 +4,14 @@
 # Biblioteca pandas para carregar arquivo csv em dataframes.
 import pandas as pd
 import numpy as np
+from sklearn.svm import SVC
 
+# Para reformatar coordenadas.
+def changeCoord(x):
+    x = x.strip('()')
+    return x.split(";")
+
+#Efetua leitura do arquivo csv.
 def read_csv():
     "Leitura das colunas: nome do arquivo, rótulo, 56 colunas de coordenadas."
     all_cols = list(range(0,65))
@@ -14,7 +21,7 @@ def read_csv():
 
 df = read_csv()
 
-# Renomear colunas
+# Renomeia colunas
 new_cols = []
 new_cols.append('file_name')
 new_cols.append('label')
@@ -22,18 +29,31 @@ for i in range(1, len(df.columns)-1):
     new_cols.append('p' + str(i))
 df.columns = new_cols
 
-# Fazer o split das coordenadas, substituindo o formato (x1; y1) por x1 y1
+# Faz o split das coordenadas, substituindo o formato (x1;y1) por x1 y1
 for col in df.columns[2:]:
-    s = df[col].apply(lambda x: x.split('; '))
-    df['x'+col[1:]] = s.apply(lambda x: x[0].strip('('))
-    df['y'+col[1:]] = s.apply(lambda x: x[1].strip(')'))
+    s = df[col].apply(changeCoord)
+    df['x'+col[1:]] = s.apply(lambda x: x[0])
+    df['y'+col[1:]] = s.apply(lambda x: x[1])
     del df[col]
 
-# Divisão do dataset em treinamento (80%) e validação (20%)
-msk = np.random.rand(len(df)) < 0.8
-training = df[msk]
-testing = df[~msk]
+class_score = []
+for i in range(0, 20):
+    # Divisão do dataset em treinamento (80%) e validação (20%)
+    msk = np.random.rand(len(df)) < 0.80
+    training = df[msk]
+    testing = df[~msk]
 
-# Para sklearn:
-testing['label'].as_matrix()
-testing.ix[:,'x1':].as_matrix()
+    # Seleciona sub-arrays e prepara para uso na sklearn
+    training_set = training.loc[:,'x1':].as_matrix()
+    training_lbls = training['label'].as_matrix()
+    testing_set = testing.loc[:,'x1':].as_matrix()
+    testing_lbls = testing['label'].as_matrix()
+    classifier = SVC(kernel='linear')
+    classifier.fit(training_set, training_lbls)
+
+    # Retorna o score da classificação em relação aos rótulos.
+    score = classifier.score(testing_set, testing_lbls)
+    class_score.append(score)
+    print("#{0:2} run score = {1:.2f}".format(i+1, score))
+
+print(("Mean value after 20 runs: {0:.2f}".format(np.mean(class_score))))
